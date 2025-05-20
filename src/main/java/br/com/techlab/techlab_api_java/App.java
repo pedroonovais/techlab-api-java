6a8e694f316f5d051b39384c5f9996d6a2e2f591
pedroonovais <phenrique101007@gmail.com>
2025-05-20 20:31:59 -0300

    remove cache and add dependency in pom

--- src/main/java/br/com/techlab/techlab_api_java/App.java
+++ src/main/java/br/com/techlab/techlab_api_java/App.java
@@ -2,14 +2,12 @@ package br.com.techlab.techlab_api_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@EnableCaching
 @OpenAPIDefinition(
 	info = @Info(
 		title = "API Tech Lab",
