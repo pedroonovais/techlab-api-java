0edbc56c487ff6d8f96f3cb34699f4904fabf891
pedroonovais <phenrique101007@gmail.com>
2025-05-20 22:40:15 -0300

    update moto filter

--- src/main/java/br/com/techlab/techlab_api_java/model/MotoFilter.java
+++ src/main/java/br/com/techlab/techlab_api_java/model/MotoFilter.java
@@ -2,6 +2,6 @@ package br.com.techlab.techlab_api_java.model;
 
 import java.time.LocalDate;
 
-public record ModelFilter(String marca, String placa, String modelo, String chassi, String motor, 
+public record MotoFilter(String marca, String placa, String modelo, String chassi, String motor, 
     String imeiIot, LocalDate dataCadastroInicial, LocalDate dataCadastroFinal) {
 }
